16f67a8a33782518a6ee731f1ca2a87656beea26
Равиль Мухаметшин <muhametshin@legaldoc.ai>
2026-04-07 01:03:25 +0400

    bugfix: Исправлена ошибка Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/dao/Storage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/Storage.java
@@ -1,13 +1,15 @@
 package ru.yandex.practicum.filmorate.dao;
 
 import ru.yandex.practicum.filmorate.model.BaseModel;
-import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.List;
 
 public interface Storage<T extends BaseModel> {
     public T create(T base);
+
     public T update(T base);
+
     public void delete(Integer index);
+
     public List<T> getList();
 }
