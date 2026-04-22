fc97ed19e75aede92355138df649dae5f8520629
Ravil <mravilal12@gmail.com>
2026-04-23 02:58:40 +0400

    bugfix: Исправлена проблема codestyle

--- src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
@@ -1,16 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
-
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Builder
+@RequiredArgsConstructor
 public class ErrorResponse {
-    // название ошибки
     private final String error;
-    // подробное описание
     private final String description;
+
     public String getError() {
         return error;
     }
